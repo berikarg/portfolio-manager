7b76440ed86f5df23f0233a24d6013763aed8d21
Berik Argimbayev <b.argimbayev@darwintrade.kz>
2022-05-09 13:18:56 +0600

    add calls from main

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,20 @@
-package cmd
+package main
+
+import (
+	portfolio_manager "github.com/berikarg/portfolio-manager"
+	"github.com/berikarg/portfolio-manager/pkg/handler"
+	"github.com/berikarg/portfolio-manager/pkg/repository"
+	"github.com/berikarg/portfolio-manager/pkg/service"
+	"log"
+)
+
+func main() {
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	srv := new(portfolio_manager.Server)
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+		log.Fatal(err)
+	}
+}
