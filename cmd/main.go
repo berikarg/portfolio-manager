47d68dbd209eea9d243d85ca64e2086c49b23d87
Berik Argimbayev <b.argimbayev@darwintrade.kz>
2022-05-09 13:33:51 +0600

    add config file and initialization

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,27 @@ import (
 	"github.com/berikarg/portfolio-manager/pkg/handler"
 	"github.com/berikarg/portfolio-manager/pkg/repository"
 	"github.com/berikarg/portfolio-manager/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("initialize configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(portfolio_manager.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
