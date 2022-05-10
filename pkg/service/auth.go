34d8111cc9050f4802ff1be59c1964fac69974b1
Berik Argimbayev <b.argimbayev@darwintrade.kz>
2022-05-10 10:21:52 +0600

    add password hashing

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -1,9 +1,13 @@
 package service
 
 import (
+	"crypto/sha1"
+	"fmt"
 	"github.com/berikarg/portfolio-manager/models"
 	"github.com/berikarg/portfolio-manager/pkg/repository"
 )
+
+const salt = "irugbN[WIOGH394GREO"
 
 type AuthService struct {
 	repo repository.Authorization
@@ -14,5 +18,13 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 }
 
 func (s *AuthService) CreateUser(user models.User) (int, error) {
+	user.Password = generatePasswordHash(user.Password)
 	return s.repo.CreateUser(user)
+}
+
+func generatePasswordHash(password string) string {
+	hash := sha1.New()
+	hash.Write([]byte(password))
+
+	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
 }
